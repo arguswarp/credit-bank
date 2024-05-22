aa5495de8242fbcc98770dc7f060133f6bc2c375
ArgusWarp <arguswarp@gmail.com>
2024-05-22 22:57:28 +0300

    api methods in controller

--- calculator/src/main/java/com/argus/calculator/controller/CalculatorController.java
+++ calculator/src/main/java/com/argus/calculator/controller/CalculatorController.java
@@ -1,4 +1,33 @@
 package com.argus.calculator.controller;
 
+import com.argus.calculator.dto.CreditDto;
+import com.argus.calculator.dto.LoanOfferDto;
+import com.argus.calculator.dto.LoanStatementRequestDto;
+import com.argus.calculator.dto.ScoringDataDto;
+import com.argus.calculator.service.CalculationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("calculator")
 public class CalculatorController {
+
+    private final CalculationService calculationService;
+
+    @PostMapping("offers")
+    @ResponseStatus(HttpStatus.OK)
+    public List<LoanOfferDto> sendOffers(@Valid @RequestBody LoanStatementRequestDto loanStatementRequestDto) {
+        return List.of();
+    }
+
+    @PostMapping("calc")
+    @ResponseStatus(HttpStatus.OK)
+    public CreditDto sendCredit(@Valid @RequestBody ScoringDataDto scoringDataDto) {
+        return new CreditDto();
+    }
 }
