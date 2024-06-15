5ab8e461409fa93aba8933f235e3ee262d15e089
ArgusWarp <arguswarp@gmail.com>
2024-06-15 11:44:47 +0300

    init statement mc

---
+++ statement/src/main/java/com/argus/statement/StatementApplication.java
@@ -0,0 +1,16 @@
+package com.argus.statement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * StatementApplication.
+ *
+ * @author Maxim Chistyakov
+ */
+@SpringBootApplication
+public class StatementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StatementApplication.class);
+    }
+}
