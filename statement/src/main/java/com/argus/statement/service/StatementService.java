facf45933170f4e63923f55d61971a6f377d99fc
ArgusWarp <arguswarp@gmail.com>
2024-06-16 12:44:22 +0300

    StatementService test impl

--- statement/src/main/java/com/argus/statement/service/StatementService.java
+++ statement/src/main/java/com/argus/statement/service/StatementService.java
@@ -5,6 +5,7 @@ import com.argus.statement.dto.LoanStatementRequestDto;
 import com.argus.statement.exception.DealApiException;
 import feign.FeignException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -17,23 +18,26 @@ import java.util.List;
  */
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class StatementService {
 
     private final DealFeignClient dealFeignClient;
 
     public List<LoanOfferDto> getLoanOffers(LoanStatementRequestDto loanStatementRequestDto) {
         try {
+            log.info("Executing POST /deal/statement: ");
             return dealFeignClient.getLoanOffers(loanStatementRequestDto);
         } catch (FeignException e) {
-            throw new DealApiException(e.responseBody().get().array(), HttpStatus.valueOf(e.status()));
+            throw new DealApiException(e.responseBody().orElseThrow(()-> new RuntimeException("Empty error response from Deal")).array(), HttpStatus.valueOf(e.status()));
         }
     }
 
     public void selectOffer(LoanOfferDto loanOfferDto) {
         try {
+            log.info("Executing POST /deal/offer/select");
             dealFeignClient.selectOffer(loanOfferDto);
         } catch (FeignException e) {
-            throw new DealApiException(e.responseBody().get().array(), HttpStatus.valueOf(e.status()));
+            throw new DealApiException(e.responseBody().orElseThrow(()-> new RuntimeException("Empty error response from Deal")).array(), HttpStatus.valueOf(e.status()));
         }
     }
 }
--- statement/src/test/java/com/argus/statement/service/StatementServiceTest.java
+++ statement/src/test/java/com/argus/statement/service/StatementServiceTest.java
@@ -1,10 +1,25 @@
 package com.argus.statement.service;
 
+import com.argus.statement.dto.LoanOfferDto;
+import com.argus.statement.dto.LoanStatementRequestDto;
+import com.argus.statement.exception.DealApiException;
+import feign.FeignException;
+import feign.Request;
+import feign.Response;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 /**
  * StatementServiceTest
@@ -20,11 +35,82 @@ class StatementServiceTest {
     @Mock
     private DealFeignClient dealFeignClient;
 
+    private static final FeignException FEIGN_EXCEPTION = FeignException.errorStatus("Get loan offers",
+            Response.builder()
+                    .status(404)
+                    .headers(new HashMap<>())
+                    .reason("Error in Deal")
+                    .request(Request.create(
+                            Request.HttpMethod.POST,
+                            "http://localhost:8081",
+                            Map.of(),
+                            null,
+                            null,
+                            null
+                    ))
+                    .build());
+
     @Test
     void getLoanOffers() {
+        when(dealFeignClient.getLoanOffers(any(LoanStatementRequestDto.class))).thenReturn(List.of(LoanOfferDto.builder()
+                        .requestedAmount(BigDecimal.TEN)
+                        .term(36)
+                        .rate(BigDecimal.TEN)
+                .build()));
+
+        List<LoanOfferDto> offers = statementService.getLoanOffers(LoanStatementRequestDto.builder().build());
+
+        assertNotNull(offers);
+        assertEquals(1, offers.size());
+        assertEquals(BigDecimal.TEN, offers.get(0).getRequestedAmount());
+        assertEquals(BigDecimal.TEN, offers.get(0).getRate());
+        assertEquals(36, offers.get(0).getTerm());
+
+        verify(dealFeignClient).getLoanOffers(any(LoanStatementRequestDto.class));
+    }
+
+    @Test
+    void whenFeignExceptionOccuredWhileGetOffers_ThenThrowDealApiException() {
+        when(dealFeignClient.getLoanOffers(any(LoanStatementRequestDto.class)))
+                .thenThrow(FEIGN_EXCEPTION);
+
+        assertThrows(DealApiException.class, () -> statementService.getLoanOffers(LoanStatementRequestDto.builder().build()));
+
+        verify(dealFeignClient).getLoanOffers(any(LoanStatementRequestDto.class));
+    }
+
+    @Test
+    void whenEmptyBodyInErrorResponseWhileGetOffers_ThenThrowRuntimeException() {
+        when(dealFeignClient.getLoanOffers(any(LoanStatementRequestDto.class)))
+                .thenThrow(FeignException.class);
+
+        assertThrows(RuntimeException.class, () -> statementService.getLoanOffers(LoanStatementRequestDto.builder().build()));
+
+        verify(dealFeignClient).getLoanOffers(any(LoanStatementRequestDto.class));
     }
 
     @Test
     void selectOffer() {
+        dealFeignClient.selectOffer(LoanOfferDto.builder().build());
+
+        verify(dealFeignClient).selectOffer(any(LoanOfferDto.class));
+    }
+
+    @Test
+    void dealApiExceptionOccuredWhileSelectOffers_ThenThrowDealApiException() {
+        doThrow(FEIGN_EXCEPTION).when(dealFeignClient).selectOffer(any(LoanOfferDto.class));
+
+        assertThrows(DealApiException.class, () -> statementService.selectOffer(LoanOfferDto.builder().build()));
+
+        verify(dealFeignClient).selectOffer(any(LoanOfferDto.class));
+    }
+
+    @Test
+    void whenEmptyBodyInErrorResponseWhileSelectOffer_ThenThrowRuntimeException() {
+        doThrow(FeignException.class).when(dealFeignClient).selectOffer(any(LoanOfferDto.class));
+
+        assertThrows(RuntimeException.class, () -> statementService.selectOffer(LoanOfferDto.builder().build()));
+
+        verify(dealFeignClient).selectOffer(any(LoanOfferDto.class));
     }
 }
